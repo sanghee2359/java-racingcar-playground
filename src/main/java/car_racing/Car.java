e07c9ed6ad3801e49f9a6c32a55747555651f08d
wjdtk <wjdtkdgml7352@naver.com>
2024-08-14 13:19:29 +0900

    feat. 랜덤숫자에 따라 레이스 점수에 반영

--- src/main/java/car_racing/Car.java
+++ src/main/java/car_racing/Car.java
@@ -5,14 +5,20 @@ import java.util.Random;
 public class Car {
     private final String name;
     private final int randomNum;
+    private final int raceScore;
 
     public Car(String name) {
         this.name = name;
         this.randomNum = makeRandomNum();
+        this.raceScore = 0;
     }
     public int makeRandomNum() {
         Random random = new Random();
         return random.nextInt(9)+1; // 1~9
+    }
+    public int getCurrentScore(int randomNum, int raceScore) {
+        if(randomNum >= 4) raceScore++;
+        return raceScore;
     }
     public int getRandomNum() {
         return this.randomNum;
--- src/test/java/car_racing/CarTest.java
+++ src/test/java/car_racing/CarTest.java
@@ -1,19 +1,29 @@
 package car_racing;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class CarTest {
+    Car car1, car2;
+    @BeforeEach
+    void setUp() {
+        car1 = new Car("pobi");
+        car2 = new Car("crong");
+    }
     @Test
     @DisplayName("자동차마다 갖게될 랜덤숫자 생성")
     void car_randomNum() {
-        Car car1 = new Car("pobi");
-        Car car2 = new Car("crong");
-
         assertThat(car1.getRandomNum()).isNotZero();
         assertThat(car2.getRandomNum()).isNotZero();
+    }
+    @Test
+    @DisplayName("자동차의 랜덤숫자가 4 이상일 때 점수에 축적")
+    void car_addScore() {
+        assertThat(car1.getCurrentScore(2, 0)).isEqualTo(0);
+        assertThat(car2.getCurrentScore(4,2)).isEqualTo(3);
     }
 
 }
